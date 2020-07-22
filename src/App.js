26c48f545e95a6400a56764a0167f278930ceff6
sgneha <sgneha05@gmail.com>
2020-07-22 13:56:33 +0100

    wrapping up in provider and creating store which is empty now

--- src/App.js
+++ src/App.js
@@ -3,28 +3,34 @@ import Posts from "./components/Posts";
 import PostForm from "./components/Postform";
 import logo from "./logo.svg";
 import "./App.css";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
+const store = createStore(() => [], {}, applyMiddleware());
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <PostForm />
-      <hr></hr>
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <PostForm />
+        <hr></hr>
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
